048f678b0b49a6552a3a08def4d360bcfbc758bd
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-10 10:36:48 +0000

    Create sum2num.c

---
+++ sum2num.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+ int num,num1,num2;
+ printf("enter number 1");
+ scanf("%d",&num1);
+ printf("enter number 2");
+ scanf("%d",&num2);
+ num=num1+num2;
+ printf("%d",num);
+ }

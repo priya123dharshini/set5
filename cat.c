331f033f22fbf839987e8c9c3a39680e33f1a27f
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-10 11:10:46 +0000

    Create cat.c

---
+++ cat.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+ char s1,s2,s;
+ printf("enter string 1");
+ scanf("%d",&s1);
+ printf("enter string 2");
+  scanf("%d",&s2);
+  s=s1+s2;
+  printf("the concatened string is %s",s);
+  }
+  

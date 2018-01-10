bf33e61b6dfd6b3b3d80c5ff2585376e079decd9
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-10 10:45:32 +0000

    Create pow2.c

---
+++ pow2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+ int n
+ printf("enter the number to be checked");
+ scanf("%d",&n);
+ if(n%2==0)
+ {
+  printf("it is a power of 2");
+ }
+ else
+ {  
+ printf("not a power of 2");
+
+} }

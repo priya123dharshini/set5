bc6c221b940e38e4e9605b08df2690412721b563
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-21 15:39:52 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+int arr max_n;
+    int i, max, min, n;
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+    printf("Enter elements in the array: ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+ }
+    
+    for(i=1; i<n; i++)
+    {
+       max==arr[0]
+       if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+
+        min==arr[0];
+  
+        if(arr[i] < min)
+        {
+            min = arr[i];
+        }
+ 
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d", min);
+
+    return 0;
+}

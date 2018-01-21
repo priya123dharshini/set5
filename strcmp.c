7c39784d94a8d18f7858c517ccd7f38553f13264
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-21 15:13:48 +0530

    Create strcmp.c

---
+++ strcmp.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+   string s1[i],s2[i];
+   int i,temp;
+   printf("enter a string-1");
+   scanf("%s",&s1[i]);
+   printf("enter a string-2");
+   scanf("%s",&s2[i]);
+   for(i=0;(s1[i]!="/0"&&s2[i]!="/0");i++)
+   {
+       if(s1[i]!=s2[i])
+       {
+        temp=1;
+        break;
+        }
+        else 
+        {
+           temp=0;
+           }
+    if(temp==0)
+    {
+      printf("the value is equal");
+    }
+    else
+    {
+    printf("the value is not equal");
+    }
+    }
+    }
+    
+    
+    
